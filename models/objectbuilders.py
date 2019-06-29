6226eae0ff9e9853b6b89bc73122e7d1fd74ea72
demxic <demxic@gmail.com>
2019-06-28 21:21:32 -0500

    feat : Add a ScheduleObjectBuilder class
    This class will be used to build all objects needed to simulate a
    crew-scheduling system
    
    All class methods receive a string or dictionary and return an object

---
+++ models/objectbuilders.py
@@ -0,0 +1,40 @@
+"""This module stores the class need to turn pbs-dictionaries into scheduleclasses objects. """
+import pytz
+
+from models.exceptions import UnsavedAirport
+from models.sheduleclasses import Airport
+
+
+class ScheduleObjectBuilder(object):
+    """This class takes a dictionary or a string and turns them into corresponding
+        schedule objects
+    """
+
+    def __init__(self, crew_position: str, trip_base: str):
+        self.crew_position = crew_position
+        self.trip_base = Airport.load_from_db(airport_iata_code=trip_base)
+
+    @staticmethod
+    def build_timezone(zone: str):
+        return pytz.timezone(zone=zone)
+
+    def build_airport(self, airport_iata_code: str) -> Airport:
+        try:
+            stored_airport = Airport.load_from_db(airport_iata_code=airport_iata_code)
+        except UnsavedAirport as e:
+            print(e)
+            ans = input("Want to save it Y/N ? ").capitalize()
+            if ans == 'Y':
+                zone = input("Enter timezone ")
+                print("Choose a viaticum zone "
+                      "paris, new_york, usa, low_cost, "
+                      "madrid, high_cost, border")
+                viaticum_zone = input()
+                unstored_airport = Airport(airport_iata_code=airport_iata_code,
+                                           timezone=self.build_timezone(zone=zone),
+                                           viaticum=viaticum_zone)
+                stored_airport = unstored_airport.save_to_db()
+            else:
+                raise e
+        return stored_airport
+
