ef6860847b9a56b84cbd03ea69d4a981fa845dcd
demxic <demxic@gmail.com>
2019-06-28 22:57:14 -0500

    feat : Add the UnsavedRoute exception

--- models/exceptions.py
+++ models/exceptions.py
@@ -2,6 +2,12 @@
 
 
 class UnsavedAirport(Exception):
-    def __init__(self, airport_iata_code):
+    def __init__(self, airport_iata_code: str):
         super().__init__("Error! {} airport is not stored in the database".format(airport_iata_code))
         self.airport_iata_code = airport_iata_code
+
+
+class UnsavedRoute(Exception):
+    def __init__(self, route):
+        super().__init__("route: {} is not stored in the data base".format(route))
+        self.route = route
